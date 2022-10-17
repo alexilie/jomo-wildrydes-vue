9322ad463897c9f6cb20a6936a68ef252a4a3cc9
alexilie <sorin.alex.ilie@gmail.com>
2022-10-17 18:00:56 -0400

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-2_9G8JhO6rz', // e.g. eu-east-2_uXboG5pAb
         userPoolClientId: '68omu0fiude5rr7enrjr76sclg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'eu-west-1' // e.g. us-east-2
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
